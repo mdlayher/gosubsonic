102404afc19dc9b02126640fce4ea9181a9f02bb
Matt Layher <mdlayher@gmail.com>
2014-03-10 19:32:58 -0400

    Cleanup, add SubsonicLicense

--- response.go
+++ response.go
@@ -1,8 +1,12 @@
 package gosubsonic
 
+import (
+	"time"
+)
+
 // APIContainer represents the top-level response from Subsonic
 type APIContainer struct {
-	Response APIStatusResponse `json:"subsonic-response"`
+	Response APIStatus `json:"subsonic-response"`
 }
 
 // APIError represents any errors reported by Subsonic
@@ -11,10 +15,28 @@ type APIError struct {
 	Code    int
 }
 
-// APIStatusResponse represents the current status of Subsonic
-type APIStatusResponse struct {
-	Error   APIError
+// APIStatus represents the current status of Subsonic
+type APIStatus struct {
+	// Common fields
 	Xmlns   string
 	Status  string
 	Version string
+
+	// API error - returned only when an error occurs
+	Error APIError
+
+	// License - returned only in GetLicense
+	License SubsonicLicense
+}
+
+// SubsonicLicense represents the license status of Subsonic
+type SubsonicLicense struct {
+	// Raw values
+	Valid   bool
+	Email   string
+	DateRaw string `json:"date"`
+	Key     string
+
+	// Parsed values
+	Date time.Time
 }
